0321dfb03ca1681ef8392b2cee5ad5a2797043d9
shlomota <shlomotannor@mail.tau.ac.il>
2021-03-10 00:53:33 +0200

    uploaded pic

--- simulations/run_multi.py
+++ simulations/run_multi.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 
 pulls=[[],[],[]]
 for l in range(5, 50, 10):
-    means = np.ones(l)
-    means[-1] = 0.18
-    epsilon = 0.8
+    # means = np.arange(0, 100, 100 / l) / 100
+    means = np.arange(0, l*2, 5) / 100
+    epsilon = 0.37
+    # means = np.ones(l)
+    # means[-1] = 0.18
+    # epsilon = 0.8
     delta = 0.01
     noise_var = 1
     maxpulls = 1e9
